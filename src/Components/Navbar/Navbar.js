db7ce5ea560100a33d393c6c7d05a5639ea399dc
Your Name~ <you@example.com>
2022-08-22 15:35:57 +0100

    remaining css for the navbar done and the login work

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -15,55 +15,54 @@ function Navbar() {
 
 
     return (
-        
         <div className='header'>
             <div className='navlogo'>
                 <Link to="/" className="logo">
                     <img className="logo-image" src={Logo} alt="Logo" />
-
                 </Link>
             </div>
-                <nav className={active ? 'navbar':'closed'}>
-                    <ul>
-                        <div className='closed'>
-                            <Icon className='close' icon="bi:x-lg" width="100" height="100" onClick={DisplayMenu} />
+            <nav className={active ? 'navbar' : 'closed'}>
+                <ul className='Nav-links'>
+                    <div className='closed'>
+                        <Icon className='close' icon="bi:x-lg" width="100" height="100" onClick={DisplayMenu} />
+                    
+                    </div>
+                    <li className='Nav-links'>
+                        WHAT'S ON
+                    </li>
+                    
+                    <li className='Nav-links'>
+                        BIG SCREEN EVENTS
+                    </li>
 
-                        </div>
-
-                        <li>
-                            WHAT'S ON
+                    <li className='Nav-links'>
+                        INSIDE VUE
+                    </li>
+                    <li className='Nav-links'>
+                        OFFERS
+                    </li>
+                    <li className='Nav-links'>
+                        GIFT CARDS
                         </li>
-                       
-                        <li>
-                            BIG SCREEN EVENTS
-                        </li>
-                        <li>
-                             INSIDE VUE
-                        </li>
-
-                            
-                        <li>
-                            OFFERS
-                            
-                        </li>
-                      
-                        <li>
-                            GIFT CARDS
-                        </li>
-
-                        <li>
+                    
+                </ul>
+            </nav>
+            <nav className='login-navTag'>
+                <ul className='nav-login' >
+                    <li className='nav-login'>
                             Login
-                        </li>
+                    </li>
 
-                        <li>
+                    <li className='nav-login'>
                             Join
-                        </li>
-                    </ul>
-                </nav>
+                    </li>
+                </ul>
 
+            </nav>
+            
+            
             <div className="menubar">
-                <Icon icon="charm:menu-hamburger" className="menu" onClick={DisplayMenu} width="50" height="50"/>
-
+                <Icon icon="charm:menu-hamburger" className="menu" onClick={DisplayMenu} width="50" height="50" />
             </div>
         </div>
         
