2480c12998cba75199e41e245aaab0a5683a6bfd
Your Name~ <you@example.com>
2022-08-27 22:56:23 +0100

    worked on the top films and refined the data that gets displayed on the page and added bit of styling

--- src/Components/TopFilms/Movie.js
+++ src/Components/TopFilms/Movie.js
@@ -1,25 +1,15 @@
-// import Reac, { useState, useEffect} from 'react'
-// import axios from '../Slides/axios'
-// import './Movies.scss'
 
-// function Movie({ title, fetchUlr, isLargeRow = false }) {
-//     const [movies, setMovies] = useState([]);
-//     useEffect(() => {
-//         async function fetchData() {
-//             const request = await axios.get(fetchUlr);
-//             setMovies(request.data.results);
-//             return request
-//         }
-//         fetchData();
+import './Movies.scss'
 
-//     }, [fetchUlr]);
-//        console.log(movies);
 
-//   return (
-//       <div className='movie'>
-//           <h2>{ title}</h2>
-//     </div>
-//   )
-// }
+function Movie() {
 
-// export default Movie
+  return (
+      <div className='movie'>
+      <h2>this is a title</h2>
+      
+    </div>
+  )
+}
+
+export default Movie
